ccdc0d97fec91498f25b0f08bdb0bf15eaf0fb6d
Vladimir Mumin <vovanmumin@gmail.com>
2024-03-22 11:23:09 +0300

    PinPadActivity

--- app/src/main/java/ru/iu3/fclient/MainActivity.java
+++ app/src/main/java/ru/iu3/fclient/MainActivity.java
@@ -1,9 +1,22 @@
 package ru.iu3.fclient;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import org.apache.commons.codec.DecoderException;
+import org.apache.commons.codec.binary.Hex;
+
+import java.nio.charset.StandardCharsets;
 
 import ru.iu3.fclient.databinding.ActivityMainBinding;
 
@@ -12,9 +25,13 @@ public class MainActivity extends AppCompatActivity {
     // Used to load the 'fclient' library on application startup.
     static {
         System.loadLibrary("fclient");
+        System.loadLibrary("mbedcrypto");
     }
 
     private ActivityMainBinding binding;
+
+    ActivityResultLauncher activityResultLauncher;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,14 +40,77 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        // Example of a call to a native method
-        TextView tv = binding.sampleText;
-        tv.setText(stringFromJNI());
+
+
+        int num = initRng();
+        byte[] key = randomBytes(10);
+
+        String s = "Click me";
+        byte[] testData = s.getBytes(StandardCharsets.UTF_8);
+
+        byte[] encrypted = encrypt(key, testData);
+        byte[] decrypted = encrypt(key, encrypted);
+
+        String res = new String(decrypted, StandardCharsets.UTF_8);
+        TextView tv = binding.sampleButton;
+        tv.setText(s);
+        activityResultLauncher  = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                (ActivityResultCallback<ActivityResult>) result-> {
+                    if (result.getResultCode() == Activity.RESULT_OK) {
+                        Intent data = result.getData();
+                        // обработка результата
+                        assert data!=null;
+                        String pin = data.getStringExtra("pin");
+
+                        Toast.makeText(MainActivity.this, pin, Toast.LENGTH_SHORT).show();
+
+                        synchronized (MainActivity.this){
+                            MainActivity.this.notifyAll();
+                        }
+                    }
+                });
     }
+
+    public void onButtonClick(View v)
+    {
+//        byte[] key = stringToHex("0123456789ABCDEF0123456789ABCDE0");
+//        byte[] enc = encrypt(key, stringToHex("000000000000000102"));
+//        byte[] dec = decrypt(key, enc);
+//        String s = new String(Hex.encodeHex(dec)).toUpperCase();
+//        Toast.makeText(this, "Hello", Toast.LENGTH_SHORT).show();
+
+        Intent it = new Intent(this, PinpadActivity.class);
+        startActivity(it);
+
+//        Intent it = new Intent(this, PinpadActivity.class);
+//        startActivity(it);
+//        activityResultLauncher.launch(it);
+    }
+
+
+    public static byte[] stringToHex(String s)
+    {
+        byte[] hex;
+        try
+        {
+            hex = Hex.decodeHex(s.toCharArray());
+        }
+        catch (DecoderException ex)
+        {
+            hex = null;
+        }
+        return hex;
+    }
+
 
     /**
      * A native method that is implemented by the 'fclient' native library,
      * which is packaged with this application.
      */
     public native String stringFromJNI();
+    public static native int initRng();
+    public static native byte[] randomBytes(int no);
+    public static native byte[] encrypt(byte[] key,byte[] data);
+    public static native byte[] decrypt(byte[] key, byte[] data);
 }
